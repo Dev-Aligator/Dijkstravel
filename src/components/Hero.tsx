ae5c15c311ba162fbfff515b74a5ce846384b5a9
Dev-Aligator <devaligator1004@gmail.com>
2023-08-15 20:26:06 +0700

    Adding framer-motion effects for some elements

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,24 @@ import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
 import { HiLocationMarker } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const animationVariants = {
+    hidden: {
+      opacity: 0,
+      y: -20,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+        ease: "easeOut",
+        bounce: 0.2,
+      },
+    },
+  };
   return (
     <section
       id="home"
@@ -20,30 +36,58 @@ const Hero = () => {
           </p>
         </div>
         <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            The Next <br className="sm:block hidden" />{" "}
-            <span className="text-gradient">Generation</span>{" "}
-          </h1>
+          <motion.div
+            initial={{ y: "2rem", opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 2,
+              type: "ease-in",
+            }}
+          >
+            <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+              The Next <br className="sm:block hidden" />{" "}
+              <span className="text-gradient">Generation</span>{" "}
+            </h1>
+          </motion.div>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
           </div>
         </div>
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        <motion.h1
+          className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full"
+          initial={{ y: "2rem", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 2,
+            type: "ease-in",
+          }}
+        >
           Payment Method.
-        </h1>
+        </motion.h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
         </p>
-        <div className="flexCenterSearch search-bar">
+        <motion.div
+          className="flexCenterSearch search-bar"
+          initial="hidden"
+          animate="visible"
+          variants={animationVariants}
+        >
           <HiLocationMarker color="#4066ff" size={25} />
           <input type="text" />
           <button className="searchButton">Search</button>
-        </div>
+        </motion.div>
       </div>
-      <div
+      <motion.div
         className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+        initial={{ x: "7rem", opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          duration: 2,
+          type: "ease-in",
+        }}
       >
         <img
           src={robot}
@@ -53,7 +97,7 @@ const Hero = () => {
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-      </div>
+      </motion.div>
       <div className={`ss:hidden ${styles.flexCenter}`}>
         <GetStarted />
       </div>
