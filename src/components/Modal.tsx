1c9d5f502d89ad128a5d762a1d4bdf68cc7a271d
Dev-Aligator <devaligator1004@gmail.com>
2023-09-06 23:04:02 +0700

    Finish switching function for login & signup

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,10 +8,8 @@ interface ModalProps {
 
 function Modal({ setOpenModal }: ModalProps) {
   const [showPasswd, setShowPasswd] = useState(false);
-  // const [formPage, setFormPage] = useState(true);
-  const func = () => {
-    console.log("hmi");
-  };
+  const [formPage, setFormPage] = useState(true);
+
   return (
     <>
       <div
@@ -64,74 +62,153 @@ function Modal({ setOpenModal }: ModalProps) {
           </small> */}
 
           <section className="form-container forms">
-            <div className="form login">
-              <div className="form-content">
-                <header>Login</header>
-                <form action="#">
-                  <div className="field input-field">
-                    <input
-                      type="text"
-                      // placeholder="Email"
-                      className="input"
-                      required
-                    ></input>
-                    <label className="form-label">Email Address</label>
-                  </div>
-                  <div className="field input-field">
-                    <input
-                      type={showPasswd ? "text" : "password"}
-                      // placeholder="Password"
-                      className="password"
-                      required
-                    ></input>{" "}
-                    <label className="form-label">Password</label>
-                    <i
-                      className={
-                        showPasswd
-                          ? "bx bx-show eye-icon"
-                          : "bx bx-hide eye-icon"
-                      }
-                      onClick={() => {
-                        if (showPasswd) {
-                          setShowPasswd(false);
-                        } else {
-                          setShowPasswd(true);
+            {formPage ? (
+              <div className="form login">
+                <div className="form-content">
+                  <header>Login</header>
+                  <form action="#">
+                    <div className="field input-field">
+                      <input
+                        type="text"
+                        // placeholder="Email"
+                        className="input"
+                        required
+                      ></input>
+                      <label className="form-label">Email Address</label>
+                    </div>
+                    <div className="field input-field">
+                      <input
+                        type={showPasswd ? "text" : "password"}
+                        // placeholder="Password"
+                        className="password"
+                        required
+                      ></input>{" "}
+                      <label className="form-label">Password</label>
+                      <i
+                        className={
+                          showPasswd
+                            ? "bx bx-show eye-icon"
+                            : "bx bx-hide eye-icon"
                         }
-                      }}
-                    ></i>
-                  </div>
+                        onClick={() => {
+                          if (showPasswd) {
+                            setShowPasswd(false);
+                          } else {
+                            setShowPasswd(true);
+                          }
+                        }}
+                      ></i>
+                    </div>
+                    <div className="form-link">
+                      <a href="#" className="forgot-pass">
+                        Forgot password?
+                      </a>
+                    </div>
+                    <div className="field button-field">
+                      <button>Login</button>
+                    </div>
+                  </form>
                   <div className="form-link">
-                    <a href="#" className="forgot-pass">
-                      Forgot password?
-                    </a>
+                    <span>
+                      Don't have an account?{" "}
+                      <a
+                        onClick={() => {
+                          setFormPage(false);
+                        }}
+                        className="link signup-link"
+                      >
+                        Signup
+                      </a>
+                    </span>
                   </div>
-                  <div className="field button-field">
-                    <button>Login</button>
-                  </div>
-                </form>
-                <div className="form-link">
-                  <span>
-                    Don't have an account?{" "}
-                    <a href="#" onClick={func} className="link signup-link">
-                      Signup
-                    </a>
-                  </span>
+                </div>
+                <div className="line"></div>
+                <div className="media-options">
+                  <a href="#" className="field facebook">
+                    <i className="bx bxl-facebook facebook-icon"></i>
+                    <span>Login with Facebook</span>
+                  </a>
+                </div>
+                <div className="media-options">
+                  <a href="#" className="field google">
+                    <img src={googleIcon} alt="" className="google-img"></img>
+                    <span>Login with Google</span>
+                  </a>
                 </div>
               </div>
-              <div className="line"></div>
-              <div className="media-options">
-                <a href="#" className="field facebook">
-                  <i className="bx bxl-facebook facebook-icon"></i>
-                  <span>Login with Facebook</span>
-                </a>
+            ) : (
+              <div className="form signup">
+                <div className="form-content">
+                  <header>Signup</header>
+                  <form action="#">
+                    <div className="field input-field">
+                      <input type="text" className="input" required></input>
+                      <label className="form-label">Email Address</label>
+                    </div>
+                    <div className="field input-field">
+                      <input
+                        type={showPasswd ? "text" : "password"}
+                        className="password"
+                        required
+                      ></input>
+                      <label className="form-label">Password</label>
+                    </div>
+                    <div className="field input-field">
+                      <input
+                        type={showPasswd ? "text" : "password"}
+                        className="password"
+                        required
+                      ></input>
+                      <label className="form-label">Confirm password</label>
+
+                      <i
+                        className={
+                          showPasswd
+                            ? "bx bx-show eye-icon"
+                            : "bx bx-hide eye-icon"
+                        }
+                        onClick={() => {
+                          if (showPasswd) {
+                            setShowPasswd(false);
+                          } else {
+                            setShowPasswd(true);
+                          }
+                        }}
+                      ></i>
+                    </div>
+                    <div className="field button-field">
+                      <button>Signup</button>
+                    </div>
+                  </form>
+                  <div className="form-link">
+                    <span>
+                      Already have an account?{" "}
+                      <a
+                        onClick={() => {
+                          setFormPage(true);
+                        }}
+                        className="link login-link"
+                      >
+                        Login
+                      </a>
+                    </span>
+                  </div>
+                </div>
+                <div className="line"></div>
+                <div className="media-options">
+                  <a href="#" className="field facebook">
+                    <i className="bx bxl-facebook facebook-icon"></i>
+                    <span>Login with Facebook</span>
+                  </a>
+                </div>
+                <div className="media-options">
+                  <a href="#" className="field google">
+                    <img src={googleIcon} alt="" className="google-img"></img>
+                    <span>Login with Google</span>
+                  </a>
+                </div>
               </div>
-              <div className="media-options">
-                <a href="#" className="field google">
-                  <img src={googleIcon} alt="" className="google-img"></img>
-                  <span>Login with Google</span>
-                </a>
-              </div>
-            </div>
+            )}
           </section>
         </div>
       </div>
